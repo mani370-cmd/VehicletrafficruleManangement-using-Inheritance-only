9f2716c346f106f46657d17d2dad979b1ec180f4
Manikandan <159552902+mani370-cmd@users.noreply.github.com>
2025-05-27 18:45:10 +0530

    Create IFine.cs

---
+++ IFine.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle_Management
+{
+    public interface IFine
+    {
+        public void DisplayFineInfo();
+    }
+}
